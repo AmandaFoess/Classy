82d9da302ea5b1c2cddcb9fa4e6d5a4393411f6c
evyshen <134675564+evyshen@users.noreply.github.com>
2024-06-06 23:00:42 -0700

    add friends in default user structure
    add friends in default user structure

--- pages/Authentication/signUpPage.js
+++ pages/Authentication/signUpPage.js
@@ -40,6 +40,12 @@ const SignUp = (e) => {
             rank: 5.0,
             professorName: "John Doe",
           }],
+          // friends: [{
+          //   #friendsids
+          // }]
+          // friendRequests: [{
+          //   #friendsids
+          // }]
         };
         await setDoc(doc(db, "Users", user.email.split("@")[0]), data);
       } catch (err) {
@@ -147,6 +153,158 @@ const styles = StyleSheet.create({
     color: "#fff",
     textAlign: "center",
   },
-});
+// });
 
-export default SignUp;
+// // export default SignUp;
+// import { useState } from "react";
+// import { Pressable, TextInput, Text, View, StyleSheet } from "react-native";
+// import { auth, db } from "../../firebase";
+// import { AuthErrorCodes, createUserWithEmailAndPassword } from "firebase/auth";
+// import { setDoc, doc } from "firebase/firestore";
+
+// const SignUp = (e) => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState(null);
+
+//   // handle form submit
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setError("");
+
+//     // creating a new user
+//     try {
+//       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+//       const user = userCredential.user;
+//       setError("");
+//       try {
+//         const data = {
+//           uid: user.uid,
+//           email: user.email,
+//           myClasses: [{
+//             course: "Example Course",
+//             quarterYearOffered: "Fall 2023",
+//             rank: 5.0,
+//             professorName: "John Doe",
+//           }],
+//           classesToTake: [{
+//             course: "Example Course",
+//             rank: 5.0,
+//             professorName: "John Doe",
+//           }],
+//           recsForYou: [{
+//             course: "Example Course",
+//             rank: 5.0,
+//             professorName: "John Doe",
+//           }],
+//           friends: ["agfoess"],
+//           friendRequests: ["agfoess"]
+//         };
+//         await setDoc(doc(db, "Users", user.uid), data);
+//       } catch (err) {
+//         setError(err.message);
+//       }
+//     } catch (error) {
+//       if (error.code === AuthErrorCodes.WEAK_PASSWORD) {
+//         setError("The password is too weak.");
+//       } else if (error.code === AuthErrorCodes.EMAIL_EXISTS) {
+//         setError("The email address is already in use.");
+//       } else {
+//         console.log(error.code);
+//         alert(error.code);
+//       }
+//       setError(error.message);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={[styles.classee, styles.classeeClr]}>Classy</Text>
+//       <View style={styles.inputContainer}>
+//         <View style={styles.inputWrapper}>
+//           <Text style={styles.label}>Email:</Text>
+//           <TextInput
+//             value={email}
+//             onChangeText={setEmail}
+//             keyboardType="email-address"
+//             autoCapitalize="none"
+//             required
+//             style={styles.input}
+//           />
+//         </View>
+//         <View style={styles.inputWrapper}>
+//           <Text style={styles.label}>Password:</Text>
+//           <TextInput
+//             onChangeText={setPassword}
+//             value={password}
+//             secureTextEntry
+//             required
+//             style={styles.input}
+//           />
+//         </View>
+//         {error ? <Text style={styles.errorText}>{error}</Text> : null}
+//         <Pressable onPress={handleSubmit} style={styles.button}>
+//           <Text style={styles.buttonText}>Create Account</Text>
+//         </Pressable>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: 20,
+//   },
+//   classeeClr: {
+//     color: "#000",
+//     fontFamily: "Inter-Regular",
+//   },
+//   classee: {
+//     fontSize: 40,
+//     textAlign: "center",
+//     marginBottom: 40,
+//   },
+//   inputContainer: {
+//     width: "100%",
+//     alignItems: "center",
+//   },
+//   inputWrapper: {
+//     width: "80%",
+//     marginBottom: 20,
+//     alignItems: "center",
+//   },
+//   label: {
+//     marginBottom: 10,
+//     textAlign: "center",
+//   },
+//   input: {
+//     height: 40,
+//     width: "100%",
+//     backgroundColor: "#d9d9d9",
+//     borderRadius: 30,
+//     paddingHorizontal: 10,
+//     textAlign: "center",
+//   },
+//   errorText: {
+//     color: "red",
+//     marginBottom: 20,
+//     textAlign: "center",
+//   },
+//   button: {
+//     backgroundColor: "#007AFF",
+//     borderRadius: 30,
+//     padding: 10,
+//     width: "80%",
+//     alignItems: "center",
+//   },
+//   buttonText: {
+//     color: "#fff",
+//     textAlign: "center",
+//   },
+// });
+
+// export default SignUp;
